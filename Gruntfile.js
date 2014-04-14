7ee5b120e77f4fa5a24ff87e3f0d59687ea75975
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-04-14 18:17:11 +0200

    Added `grunt-contrib-connect`

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,6 +11,7 @@ module.exports = function ( grunt ) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-ngmin');
     grunt.loadNpmTasks('grunt-html2js');
 
