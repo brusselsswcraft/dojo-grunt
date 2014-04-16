ad682aee77dc8f44abd72360c061a8a9311678bc
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-04-16 10:31:11 +0200

    Removed the translation dependencies

--- build.config.js
+++ build.config.js
@@ -56,9 +56,7 @@ module.exports = {
             'vendor/angular/angular.js',
             'vendor/angular-route/angular-route.js',
             'vendor/angular-sanitize/angular-sanitize.js',
-            'vendor/angular-bootstrap/ui-bootstrap-tpls.min.js',
-            'vendor/angular-translate/angular-translate.js',
-            'vendor/angular-translate-loader-static-files/angular-translate-loader-static-files.js'
+            'vendor/angular-bootstrap/ui-bootstrap-tpls.min.js'
         ],
         css: [
         ],
